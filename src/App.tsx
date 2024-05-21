a53da01f1a53f973b26e9438df3e916b939cfc31
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2024-05-21 16:46:50 -0300

    Configuração de tema da MUI adicionada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { AppBar } from '@mui/material'
-import SearchAppBar from './components/Header/index1'
-import Header from './components/Header'
+import { AppBar, Button } from '@mui/material'
+// import SearchAppBar from './components/Header/index1'
+// import Header from './components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AppThemeProvider } from './shared/contexts'
+import { AppRoutes } from './routes';
+
+
+// Pinterest red: #cb2027
 
 export default function App() {
 
   return (
-    <div className='App'>
-      <Header />
-    </div>
+    <AppThemeProvider>
+      <BrowserRouter>
+        {/* <Header />
+        <Routes>
+          <Route path="/" element={ <Button variant="contained" color='primary' onClick={() => { toggleTheme }}> Página Inicial </Button> } />
+          <Route path="/explore" element={<p> Explorar </p>} />
+        </Routes>*/}
+        <AppRoutes />
+      </BrowserRouter> 
+    </AppThemeProvider>
   )
 }
