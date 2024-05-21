d0139863e623b15d70a02f2fd281bb68ae38feb3
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2024-05-21 12:12:53 -0300

    Organizando arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { AppBar } from '@mui/material'
 import SearchAppBar from './components/Header/index1'
 import Header from './components/Header'
@@ -6,8 +5,8 @@ import Header from './components/Header'
 export default function App() {
 
   return (
-    <>
+    <div className='App'>
       <Header />
-    </>
+    </div>
   )
 }
